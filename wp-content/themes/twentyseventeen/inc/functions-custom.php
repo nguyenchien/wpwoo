d7ad8fc24ac1fd511bd6b1d87f008fda3b6cf1db
nguyenchien <chiennguyen1702@gmail.com>
2018-11-05 22:38:30 +0700

    remove srcset image

--- wp-content/themes/twentyseventeen/inc/functions-custom.php
+++ wp-content/themes/twentyseventeen/inc/functions-custom.php
@@ -1,4 +1,10 @@
 <?php
+// Remove srcset image
+function meks_disable_srcset( $sources ) {
+    return false;
+}
+add_filter( 'wp_calculate_image_srcset', 'meks_disable_srcset' );
+
 // Disable PostCode/Zip Field on the Checkout Page
 add_filter( 'woocommerce_default_address_fields' , 'chien_custom_checkout_fields' );
 function chien_custom_checkout_fields( $fields ) {
